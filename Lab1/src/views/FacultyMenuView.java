6f6dc808f77e4a3e1c5b190b926798fa147a6db2
Tudor Popov <supreme.notfound@gmail.com>
2023-10-05 01:53:02 +0300

    added the option to go back from faculty menu to main menu

--- Lab1/src/views/FacultyMenuView.java
+++ Lab1/src/views/FacultyMenuView.java
@@ -1,9 +1,11 @@
 package views;
 
+import com.sun.tools.javac.Main;
 import utils.Faculty;
 import utils.University;
 
 import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 import static helpers.ConsoleHelper.clearConsole;
@@ -13,6 +15,7 @@ public class FacultyMenuView {
     private Faculty faculty;
     private University university;
     private int options;
+
     FacultyMenuView(){
         this.scanner = new Scanner(System.in);
         this.faculty = new Faculty();
@@ -26,6 +29,7 @@ public class FacultyMenuView {
         System.out.println("4. Give a list with all the students of a faculty");
         System.out.println("\n 0. Go Back");
         this.options = scanner.nextInt();
+        while (options != 0){
         switch (options){
             case 1: clearConsole();
                     addFaculty();
@@ -34,6 +38,18 @@ public class FacultyMenuView {
                     break;
             case 2: clearConsole();
                     university.getFaculties();
+                System.out.println("These are all the faculties. (press 1 - to continue)");
+                if(scanner.nextInt() == 1){
+                    run();
+                }break;
+            case 0: clearConsole();
+                System.out.println("TUM - Management System");
+                System.out.println("Chose What you want to do");;
+                System.out.println("1. Faculty Options");
+                System.out.println("2. Student Options");
+                System.out.println("0. Quit");
+                    break;
+            }
         }
     }
 
